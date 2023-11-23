f9cc6087534da3825c4f0fd222a77ccf33e63ccc
hyeonseo <s21035@gsm.hs.kr>
2023-11-23 09:19:32 +0900

    :lipstick: :: 잘못된 색상값 수정

--- webApp/src/jsMain/kotlin/designsystem/theme/Color.kt
+++ webApp/src/jsMain/kotlin/designsystem/theme/Color.kt
@@ -4,6 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 val Red = Color(0xFFEB5757)
 val Gray = Color(0xFF787774)
-val LightPurple = Color(0xF4F0F7CC)
-val LightBrown = Color(0x87837826)
+val LightPurple = Color(0xCCF4F0F7)
+val LightBrown = Color(0x26878378)
 val LightGray = Color(0xFFF1F1EF)
