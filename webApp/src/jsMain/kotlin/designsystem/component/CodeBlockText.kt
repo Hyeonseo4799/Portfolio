055f709138120810d95ac351b4a8c3bcf830ce50
hyeonseo <s21035@gsm.hs.kr>
2023-11-27 10:59:26 +0900

    :zap: :: annotatedString & 정규 표현식을 통해 구현되도록 로직 변경

--- webApp/src/jsMain/kotlin/designsystem/component/CodeBlockText.kt
+++ webApp/src/jsMain/kotlin/designsystem/component/CodeBlockText.kt
@@ -1,53 +1,104 @@
 package designsystem.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.ui.text.AnnotatedString.Builder
+import androidx.compose.foundation.text.appendInlineContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.*
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.em
 import designsystem.theme.PortfolioTheme
 
-@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun CodeBlockText(
-    modifier: Modifier = Modifier,
-    fullText: String,
-    highlightedWords: List<String>,
-    boldCodeBlock: Boolean = false,
+    text: String,
 ) {
-    val words = fullText.split(" ")
+    val widthTextUnits = remember { mutableStateListOf<TextUnit>() }
 
-    // TODO: Lazy 하게 표시되도록 변경하기
-    FlowRow(modifier = modifier) {
-        words.forEach { word ->
-            val isCodeBlock = word in highlightedWords
+    val annotatedString = buildAnnotatedString {
+        appendCodeBlockText(text = text) { value ->
+            widthTextUnits.add(calculatePaddedWidth(value.size.width))
+        }
+    }
 
+    val placeholders = widthTextUnits.map {
+        Placeholder(
+            width = it,
+            height = 1.5.em,
+            placeholderVerticalAlign = PlaceholderVerticalAlign.TextCenter
+        )
+    }
+
+    val inlineTextContent = placeholders.mapIndexed { index, placeholder ->
+        "code$index" to InlineTextContent(placeholder = placeholder) {
             Text(
                 modifier = Modifier
                     .background(
-                        color = if (isCodeBlock) PortfolioTheme.color.blockBrown else Color.Transparent,
+                        color = PortfolioTheme.color.blockBrown,
                         shape = RoundedCornerShape(3.dp)
                     )
-                    .padding(
-                        horizontal = if (isCodeBlock) 6.4.dp else 0.dp,
-                        vertical = if (isCodeBlock) 3.2.dp else 0.dp
-                    )
-                    .align(Alignment.CenterVertically),
-                text = " $word ",
-                fontSize = if (isCodeBlock) 13.6.sp else 16.sp,
-                style = MaterialTheme.typography.body1,
-                fontWeight = if (isCodeBlock && boldCodeBlock) FontWeight.Bold else FontWeight.Normal,
-                color = if (isCodeBlock) PortfolioTheme.color.red else Color.Black
+                    .padding(horizontal = 6.4.dp),
+                text = it,
+                color = PortfolioTheme.color.red
             )
         }
+    }.toMap()
+
+    Text(
+        text = annotatedString,
+        inlineContent = inlineTextContent,
+        style = MaterialTheme.typography.body1
+    )
+}
+
+@Composable
+private fun Builder.appendCodeBlockText(
+    text: String,
+    onTextMeasure: @Composable (TextLayoutResult) -> Unit,
+) {
+    val codeBlockRegex = "`(.+?)`".toRegex()
+    val textMeasurer = rememberTextMeasurer()
+
+    var currentIndex = 0
+
+    codeBlockRegex.findAll(text).forEachIndexed { index, result ->
+        val codeBlock = result.groupValues[1]
+        val startIndex = result.range.first
+        val endIndex = result.range.last + 1
+        val textLayoutResult = textMeasurer.measure(
+            text = codeBlock,
+            style = MaterialTheme.typography.body1,
+        )
+
+        onTextMeasure(textLayoutResult)
+
+        if (currentIndex < startIndex) {
+            append(text.substring(currentIndex, startIndex))
+        }
+
+        appendInlineContent("code$index", codeBlock)
+
+        currentIndex = endIndex
+    }
+
+    if (currentIndex < text.length) {
+        append(text.substring(currentIndex))
+    }
+}
+
+private fun calculatePaddedWidth(width: Int): TextUnit {
+    return with(LocalDensity.current) {
+        // horizontal padding(6.4.dp)을 반올림 한 값으로 설정
+        val paddingPx = 13.dp.toPx()
+        val textPx = width.toDp().toPx()
+        (paddingPx + textPx).toSp()
     }
 }
